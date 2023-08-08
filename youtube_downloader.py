0308321f904527a5f934acd5db59a5a37836e480
Phil Inzag <dev.philinzag@gmail.com>
2023-08-08 22:41:00 +0000

    Update youtube_downloader.py

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -3,7 +3,7 @@ from pytube import YouTube
 from moviepy.video.io.VideoFileClip import VideoFileClip
 
 # Define the YouTube video URL and chapter timestamps
-video_url = "Your YouTube Video URL"
+VIDEO_URL = "Your YouTube Video URL"
 chapter_timestamps = {
     "Chapter 1": {"start": "0:00", "end": "2:30"},
     "Chapter 2": {"start": "2:31", "end": "5:00"},
@@ -11,28 +11,28 @@ chapter_timestamps = {
 }
 
 # Set the download location
-download_location = "C:/Path/To/Download/Folder"
+DOWNLOAD_LOCATION = "C:/Path/To/Download/Folder"
 
 # Create the download location folder if it doesn't exist
-if not os.path.exists(download_location):
-    os.makedirs(download_location)
+if not os.path.exists(DOWNLOAD_LOCATION):
+    os.makedirs(DOWNLOAD_LOCATION)
 
 # Download the YouTube video
-yt = YouTube(video_url)
+yt = YouTube(VIDEO_URL)
 video_stream = yt.streams.get_highest_resolution()
-video_filename = video_stream.download(output_path=download_location)
+video_filename = video_stream.download(output_path=DOWNLOAD_LOCATION)
 
 # Function to split the video based on chapter timestamps
-def split_video(chapter, start_time, end_time):
-    video_clip = VideoFileClip(video_filename).subclip(start_time, end_time)
-    output_filename = os.path.join(download_location, f"{chapter}.mp4")
+def split_video(chapter, START_TIME, END_TIME):
+    video_clip = VideoFileClip(video_filename).subclip(START_TIME, END_TIME)
+    output_filename = os.path.join(DOWNLOAD_LOCATION, f"{chapter}.mp4")
     video_clip.write_videofile(output_filename)
     video_clip.close()
 
 # Loop through the chapter timestamps and split the video
 for chapter, timestamps in chapter_timestamps.items():
-    start_time = timestamps["start"]
-    end_time = timestamps["end"]
-    split_video(chapter, start_time, end_time)
+    START_TIME = timestamps["start"]
+    END_TIME = timestamps["end"]
+    split_video(chapter, START_TIME, END_TIME)
 
 print("Split and downloaded videos based on chapters!")
